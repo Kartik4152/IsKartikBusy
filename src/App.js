f3a3d25c3833de700d5b27ae5dfbb0659c85186f
Kartik4152 <gaming4152@gmail.com>
2021-02-28 17:33:44 +0530

    fixed mobile scaling

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import React,{useState,useEffect} from 'react';
+import React,{useState,useEffect,useRef} from 'react';
 import './App.css';
+
 
 const App=()=>{
   const [status,setStatus]=useState({});
+  const containerRef=useRef();
   useEffect(()=>{
     const options={
       method:'GET',
-      headers:{
-        'Content-Type':'application/json',
-        // 'Authorization':`${btoa(unescape(encodeURIComponent('your authorization string')))}`,
-      }};
+      headers:{'Content-Type':'application/json'}
+    };
     fetch('https://iskartikbusy.herokuapp.com/getStatus',options)
     .then(res=>res.json())
     .then(result=>setStatus(result))
     .catch(err=>setStatus({imgTxt:'Fixing This Shitty Website'}));
-  },[])
+    const changeHeight=()=>{
+      containerRef.current.style.height=`${window.innerHeight}px`;
+    }
+    changeHeight();
+    window.addEventListener('resize',changeHeight);
+    return ()=>{window.removeEventListener('resize',changeHeight)};
+  },[]);
   return(
-    <div className='container'>
+    <div className='container' ref={containerRef}>
       <div className='heading'>Kartik is currently : </div>
         {status.imgUrl && <img id="img" src={status.imgUrl} alt={status.description||'No Description Provided'}/>}
         <p id='imgTxt'>{status.imgTxt}</p>
